ea22ebbd81e91c5b57f372fb8d3a7886b15996b9
AkinoJoey <gakiya213@gmail.com>
2023-11-19 18:46:17 +0900

    route側でエラーコードとメッセージを管理するようにした

--- Views/component/404.php
+++ Views/component/404.php
@@ -1,10 +1,6 @@
-<?php
-http_response_code(404);
-?>
-
 <main class="container">
     <h2 class="text-center"><a class="text-decoration-none" href="/">Text Snippetter</a></h2>
     <div>
-        <p class="text-center">Snippet Expired</p>
+        <p class="text-center"><?= $errormsg ?></p>
     </div>
 </main>
