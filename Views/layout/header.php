1e9f33555838e1322879a133cb2b0ea54fc0551a
AkinoJoey <gakiya213@gmail.com>
2023-11-19 18:46:49 +0900

    publicをルートディレクトリにしたことによる変更

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css">
-    <link rel="stylesheet" href="../../public/css/style.css">
-    <link rel="icon" href="../../public/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/css/style.css" type="text/css">
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
     <title>Text Snippetter</title>
 </head>
 
